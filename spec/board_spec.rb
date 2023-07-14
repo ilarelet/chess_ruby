6401c0e93dbde3c7836b6c0301bf75b93fb5625c
Ilya Eletskiy <ilarelet@gmail.com>
2023-07-14 14:14:29 +0300

    Board cell access failure tests added

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -18,5 +18,38 @@ describe Board do
                 expect(cell).to equal(board_1.cells[0][0])
             end
         end
+
+        context 'If an invalid cell is given' do
+            it 'raises error if the index is too long' do
+                index = 'b53'
+                expect {board_1.cell(index)}.to raise_error(ArgumentError)
+            end
+            
+            it 'raises error if the index is not given' do
+                index = ''
+                expect {board_1.cell(index)}.to raise_error(ArgumentError)
+            end
+            
+            it 'raises error if the row is 0' do
+                index = 'd0'
+                expect {board_1.cell(index)}.to raise_error(ArgumentError)
+            end
+
+            it 'raises error if the row is more than 8' do
+                index = 'e9'
+                expect {board_1.cell(index)}.to raise_error(ArgumentError)
+            end
+
+            it 'raises error if the column doesn\'t exist' do
+                index = 'i6'
+                expect {board_1.cell(index)}.to raise_error(ArgumentError)
+            end
+
+
+            it 'raises error if the column code is a number' do
+                index = '36'
+                expect {board_1.cell(index)}.to raise_error(ArgumentError)
+            end
+        end
     end
 end
