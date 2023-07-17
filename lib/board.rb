e4dff8bbd723566203e9744d0c1b762915774547
Ilya Eletskiy <ilarelet@gmail.com>
2023-07-17 15:05:04 +0300

    A method finding indexes of a given cell separated

--- lib/board.rb
+++ lib/board.rb
@@ -17,8 +17,9 @@ class Board
         end
     end 
 
-    #a method to access an exact cell by it's alphanumerical code (e2, c5 etc.)
-    def cell(cell_code)
+    
+    #This method finds row and column indexes of a cell by it's alphanumerical code (e2, c5 etc.)
+    def find_indexes(cell_code)
         unless cell_code.length == 2 and cell_code[0].match /[A-Ha-h]/ and cell_code[1].match /[1-8]/
             raise ArgumentError.new "Incorrect cell index"
         end
@@ -28,8 +29,16 @@ class Board
         #translate a letter code of the column to the column index
         column_index = letter_code.upcase.ord - 64
         #get a row index from the number code
-        row_index = num_code - 1
+        row_index = num_code
+        #return the row and column index of the cell
+        [row_index, column_index]
+    end
+    
+    #a method to access an exact cell by it's alphanumerical code
+    def cell(cell_code)
+        #use #find_indexes to determine row and column indexes of the cell
+        indexes = self.find_indexes(cell_code)
         #return the requested cell
-        @cells[row_index][column_index]
+        @cells[indexes[0]][indexes[1]]
     end
 end
