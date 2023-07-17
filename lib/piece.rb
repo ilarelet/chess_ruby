d9ab245823504059137f272e0713a1d3a744042f
Ilya Eletskiy <ilarelet@gmail.com>
2023-07-17 13:59:52 +0300

    Changing location for Piece parent-class added

--- lib/piece.rb
+++ lib/piece.rb
@@ -1,7 +1,7 @@
 require_relative 'player.rb'
 
 class Piece
-    attr_accessor :location, :alive?
+    attr_accessor :location, :alive?, :possible_moves, :possible_attacks
     
     def initialize(location, player)
         @color = player.color
@@ -9,4 +9,14 @@ class Piece
         @alive? = true
     end
 
+    private
+    def change_location(cell)
+        @location = cell
+    end
+
+    #required for children of that class: 
+        #find_possible_moves
+        #find_possible_attacks
+        #move
+        #attack
 end
