225c74d54ab986c61b72781b0e353ee31d430780
Ilya Eletskiy <ilarelet@gmail.com>
2023-07-17 15:18:31 +0300

    Name added (for displaying and logging)

--- lib/pieces/King.rb
+++ lib/pieces/King.rb
@@ -2,6 +2,20 @@ require_relative "../piece.rb"
 
 class Knight < Piece
 
+    def initialize
+        @name = "King"
+    end
+
+    #move to new_cell (where new_cell is an alphanumerical code)
+    def move(new_cell)
+        #if the cell is not empty the figure
+        unless board.cell(new_cell) = nil
+            board.cell(new_cell).alive? = false
+            board.cell(new_cell).killed_message
+        end
+
+    end
+
     private
     #metod defining where a Knight can move
     def find_possible_moves
