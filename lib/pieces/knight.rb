de63ebaeb8c6fd77a3205bf1c3b772fe33adbd30
Ilya Eletskiy <ilarelet@gmail.com>
2023-07-23 22:02:38 +0300

    knight moves added

---
+++ lib/pieces/knight.rb
@@ -0,0 +1,30 @@
+require_relative "../piece.rb"
+
+class Knight < Piece
+
+    def initialize(location, player, board)
+        super
+        @name = "Knight"
+    end
+
+    private
+    #metod defining where a Knight can move
+    private
+    #metod defining where a King can move
+    def find_possible_moves
+        #figuring out current location indexes
+        current_position = @board.find_indexes(@location)
+        row_index = current_position[0]
+        column_index = current_position[1]
+        #all possible knight's moves 
+        @possible_moves = []
+        add_valid_move([row_index+2, column_index+1])
+        add_valid_move([row_index+2, column_index-1])
+        add_valid_move([row_index-2, column_index+1])
+        add_valid_move([row_index-2, column_index-1])
+        add_valid_move([row_index+1, column_index+2])
+        add_valid_move([row_index+1, column_index-2])
+        add_valid_move([row_index-1, column_index+2])
+        add_valid_move([row_index-1, column_index-2])
+    end
+end
