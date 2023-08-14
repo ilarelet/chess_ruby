cde38b14a9184e50f0bd8c6a279d58779bd3e824
Ilya Eletskiy <ilarelet@gmail.com>
2023-08-14 11:48:02 +0300

    Bishop movements described

---
+++ lib/pieces/Bishop.rb
@@ -0,0 +1,44 @@
+require_relative '../player.rb'
+require_relative "../piece.rb"
+require_relative "../board.rb"
+
+class Bishop < Piece
+
+    def initialize(location, player, board)
+        super
+        @name = "Bishop"
+    end
+
+    private
+    #metod defining where a King can move
+    def find_possible_moves
+        #figuring out current location indexes
+        current_position = @board.find_indexes(@location)
+        row_index = current_position[0]
+        column_index = current_position[1]
+        #all possible king's moves 
+        @possible_moves = []
+        
+        #travel each possible direction (4 diagonals)
+        add_valid_moves_from_line(row_index, column_index) do |row, column| 
+            row += 1
+            column += 1
+            [row, column]
+        end
+        add_valid_moves_from_line(row_index, column_index) do |row, column| 
+            row += 1
+            column -= 1
+            [row, column]
+        end
+        add_valid_moves_from_line(row_index, column_index) do |row, column| 
+            row -= 1
+            column += 1
+            [row, column]
+        end
+        add_valid_moves_from_line(row_index, column_index) do |row, column| 
+            row -= 1
+            column -= 1
+            [row, column]
+        end
+    end
+end
